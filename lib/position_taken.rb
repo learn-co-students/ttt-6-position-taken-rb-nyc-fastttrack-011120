087b2eb46731c07945c619d312ce5f74ee33fd3e
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-12 13:50:42 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here
+def position_taken?(board, index)
+
+  if board[index].include?("") || board[index].include?(" ")
+    return false
+  end
+end
+
+position_taken?([], 0)
